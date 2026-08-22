02cd7c3350e52fba9177c30c79c205200e0a0024
GuilhermeLima-SLT <guilherme.lima@solutis.com.br>
2026-08-22 18:58:29 -0300

    perf: refatorei o comportamento de geracao do id na entidade JPA de usuarios e implementado primeira versao de migration para padronizar criacao da tabela de 'users'

--- backend/user-service/user/src/main/java/br/solutis/user/entity/User.java
+++ backend/user-service/user/src/main/java/br/solutis/user/entity/User.java
@@ -8,15 +8,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.UUID;
+
 @Entity
 @Table(name = "users")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(unique = true, nullable = false)
     @NotBlank
