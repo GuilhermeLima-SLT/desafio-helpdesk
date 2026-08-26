8d5ab0718c887eb78c5de586c366c32f9d130efb
GuilhermeLima-SLT <guilherme.lima@solutis.com.br>
2026-08-26 20:49:55 -0300

    feat: Adicionada configuração inicial do servidor com configuração do servidor Eureka e Maven Wrapper

---
+++ backend/server-service/server/src/test/java/br/solutis/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.solutis.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
