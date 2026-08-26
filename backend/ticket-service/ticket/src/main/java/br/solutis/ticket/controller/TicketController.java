dee0b9664a3c331b328ca0ca9f17542126bcd346
GuilhermeLima-SLT <guilherme.lima@solutis.com.br>
2026-08-26 11:17:29 -0300

    feat: Adicionados endpoints simples para (filtragem) listar tickets por prioridade, categoria e ID do cliente (por enquanto apenas) no TicketController e TicketRepository

--- backend/ticket-service/ticket/src/main/java/br/solutis/ticket/controller/TicketController.java
+++ backend/ticket-service/ticket/src/main/java/br/solutis/ticket/controller/TicketController.java
@@ -2,6 +2,8 @@ package br.solutis.ticket.controller;
 
 import br.solutis.ticket.dto.CreateTicketRequest;
 import br.solutis.ticket.entity.Ticket;
+import br.solutis.ticket.enums.category.Category;
+import br.solutis.ticket.enums.priority.TicketPriority;
 import br.solutis.ticket.enums.status.Status;
 import br.solutis.ticket.repository.TicketRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/tickets")
@@ -38,5 +41,20 @@ public class TicketController {
     @GetMapping("/status/{status}")
     public ResponseEntity<List<Ticket>> getByStatus(@PathVariable String status) {
         return ResponseEntity.ok(ticketRepository.findByStatus(Status.valueOf(status)));
+    }
+
+    @GetMapping("/priority/{priority}")
+    public ResponseEntity<List<Ticket>> getByPriority(@PathVariable String priority) {
+        return ResponseEntity.ok(ticketRepository.findByPriority(TicketPriority.valueOf(priority)));
+    }
+
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<Ticket>> getByCategory(@PathVariable String category) {
+        return ResponseEntity.ok(ticketRepository.findByCategory(Category.valueOf(category)));
+    }
+
+    @GetMapping("/customer/{customerId}")
+    public ResponseEntity<List<Ticket>> getByCustomerId(@PathVariable UUID customerId) {
+        return ResponseEntity.ok(ticketRepository.findByCustomerId(customerId));
     }
 }
--- backend/ticket-service/ticket/src/main/java/br/solutis/ticket/repository/TicketRepository.java
+++ backend/ticket-service/ticket/src/main/java/br/solutis/ticket/repository/TicketRepository.java
@@ -1,7 +1,9 @@
 package br.solutis.ticket.repository;
 
 import br.solutis.ticket.entity.Ticket;
+import br.solutis.ticket.enums.category.Category;
 import br.solutis.ticket.enums.status.Status;
+import br.solutis.ticket.enums.priority.TicketPriority;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -10,4 +12,10 @@ import java.util.UUID;
 public interface TicketRepository extends JpaRepository<Ticket, UUID> {
 
     List<Ticket> findByStatus(Status status);
+
+    List<Ticket> findByPriority(TicketPriority priority);
+
+    List<Ticket> findByCategory(Category category);
+
+    List<Ticket> findByCustomerId(UUID customerId);
 }
