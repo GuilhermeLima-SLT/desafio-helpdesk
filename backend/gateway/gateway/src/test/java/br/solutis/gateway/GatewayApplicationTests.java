0855664dcf87d8fa187f9c1218c039d207a0bec1
GuilhermeLima-SLT <guilherme.lima@solutis.com.br>
2026-08-27 16:40:30 -0300

    feat: Implementado estrutura inicial do gateway com configuracao do Maven, incluindo arquivos de propriedades e anotacoes do Eureka-Server nas classes principais

---
+++ backend/gateway/gateway/src/test/java/br/solutis/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.solutis.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
